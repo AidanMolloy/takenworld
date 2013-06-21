13a5b71a1f4f09ab8b39be4cd93c7774cb676de8
AidanMolloy <aidan@molloy.ie>
2013-06-21 23:31:04 +0200

    1.6.0 21/6/2013

--- units.php
+++ units.php
@@ -8,21 +8,31 @@ if(!isset($_SESSION['uid'])) {
   if(isset($_POST['train'])) {
     $soldier = protect($_POST['soldier']);
     $ranger = protect($_POST['ranger']);
-    $food_needed = (10 * $soldier) + (18 * $ranger);
-    if($soldier < 0 || $ranger < 0) {
+    $tank = protect($_POST['tank']);
+    $food_needed = (10 * $soldier) + (18 * $ranger) + (15 * $tank);
+    $gold_needed = (15 * $tank);
+    if($soldier < 0 || $ranger < 0 || $tank < 0) {
       output("You must train a positive number of Units.");
     }elseif($stats['food'] < $food_needed) {
       output("You do not have enough food.");
+    }elseif($stats['gold'] < $gold_needed) {
+      output("You do not have enough gold.");
     }else{
       $unit['soldier'] += $soldier;
       $unit['ranger'] += $ranger;
+      $unit['tank'] += $tank;
       
       $update_unit = mysql_query("UPDATE `unit` SET 
                                   `soldier`='".$unit['soldier']."', 
-                                  `ranger`='".$unit['ranger']."' 
+                                  `ranger`='".$unit['ranger']."',
+                                  `tank`='".$unit['tank']."' 
                                   WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
       $stats['food'] -= $food_needed;
       $update_food = mysql_query("UPDATE `stats` SET `food`='".$stats['food']."'
+                                  WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
+                                  
+      $stats['gold'] -= $gold_needed;
+      $update_gold = mysql_query("UPDATE `stats` SET `gold`='".$stats['gold']."'
                                   WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
                                   
                                   
@@ -32,22 +42,31 @@ if(!isset($_SESSION['uid'])) {
   }elseif(isset($_POST['untrain'])) {
     $soldier = protect($_POST['soldier']);
     $ranger = protect($_POST['ranger']);
-    $food_gained = (8 * $soldier) + (13 * $ranger);
-    if($soldier < 0 || $ranger < 0) {
+    $tank = protect($_POST['tank']);
+    $food_gained = (8 * $soldier) + (13 * $ranger) + (10 * $tank);
+    $gold_gained = (10 * $tank);
+    if($soldier < 0 || $ranger < 0 || $tank < 0) {
       output("You must untrain a positive number of Units.");
-    }elseif($soldier > $unit['soldier'] || $ranger > $unit['ranger']) {
+    }elseif($soldier > $unit['soldier'] || $ranger > $unit['ranger'] || $tank > $unit['tank']) {
       output("You do not have that many Units to untrain.");
     }else{
       $unit['soldier'] -= $soldier;
       $unit['ranger'] -= $ranger;
+      $unit['tank'] -= $tank;
       
       $update_unit = mysql_query("UPDATE `unit` SET 
                                   `soldier`='".$unit['soldier']."', 
-                                  `ranger`='".$unit['ranger']."' 
+                                  `ranger`='".$unit['ranger']."',
+                                  `tank`='".$unit['tank']."'
                                   WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
       $stats['food'] += $food_gained;
       $update_food = mysql_query("UPDATE `stats` SET `food`='".$stats['food']."'
                                   WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
+                                  
+      $stats['gold'] += $gold_gained;
+      $update_gold = mysql_query("UPDATE `stats` SET `gold`='".$stats['gold']."'
+        			  WHERE `id`='".$_SESSION['uid']."'") or die(mysql_error());
+      				  
       include("update_stats.php");
       output("You have untrained your Units!");
     }
@@ -78,6 +97,15 @@ if(!isset($_SESSION['uid'])) {
       <td><input type="text" name="ranger" /></td>
     </tr>
     <tr>
+      <td>Tank</td>
+      <td><?php  echo number_format($unit['tank']); ?></td>
+      <td>
+      <img src="images/food_icon.png" alt="Logo" width="27" height="24"  title="Food"/>15
+      <img src="images/gold_icon1.png" alt="Logo" width="22" height="22"  title="Gold"/>15
+      </td>
+      <td><input type="text" name="tank" /></td>
+    </tr>
+    <tr>
     <td></td>
     <td></td>
     <td></td>
@@ -105,6 +133,15 @@ if(!isset($_SESSION['uid'])) {
       <td><?php  echo number_format($unit['ranger']); ?></td>
       <td><img src="images/food_icon.png" alt="Logo" width="27" height="24"  title="Food"/>13</td>
       <td><input type="text" name="ranger" /></td>
+    </tr>
+    <tr>
+      <td>Tank</td>
+      <td><?php  echo number_format($unit['tank']); ?></td>
+      <td>
+      <img src="images/food_icon.png" alt="Logo" width="27" height="24"  title="Food"/>10
+      <img src="images/gold_icon1.png" alt="Logo" width="22" height="22"  title="Gold"/>10
+      </td>
+      <td><input type="text" name="tank" /></td>
     </tr>
     <tr>
     <td></td>
