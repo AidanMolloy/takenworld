3eed0785b0d57c7775b31e65af5acd545f0b0564
AidanMolloy <aidan@molloy.ie>
2013-06-23 20:50:35 +0200

    1.6.5 23/06/2013

--- battle.php
+++ battle.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("header.php");
-include("user_stats");
+include("user_stats.php");
 if(!isset($_SESSION['uid'])){
     echo "You must be logged in to view this page!";
 }else{
