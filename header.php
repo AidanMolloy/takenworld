dd399835baaacfdd771cd90f435561b171376e05
AidanMolloy <aidan@molloy.ie>
2013-07-09 19:02:07 +0100

    1.7.8 09/07/2013

--- header.php
+++ header.php
@@ -1,8 +1,34 @@
-<?php 
+<?php
 include("functions.php");
 connect();
 include("user_stats.php");
+
+$maxenergy = 100; $currentenergy = $stats['energy']; $percent = round(($currentenergy/$maxenergy) * 100,1);
 ?>
+<style>
+.outterprogressbar {
+	margin-left: 456px;
+	margin-top: -26px;
+	height: 20px;	
+	width: 100px;
+	background-color: #999;
+	border: solid 1px #000;
+}
+
+.energyprogress {
+	height: 20px;
+	width: <?php echo $percent ?>%;
+	border-right: solid 1px #000;
+	background: #fefcea; /* Old browsers */
+	background: -moz-linear-gradient(top, #fefcea 0%, #f1da36 100%); /* FF3.6+ */
+	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#fefcea), color-stop(100%,#f1da36)); /* Chrome,Safari4+ */
+	background: -webkit-linear-gradient(top, #fefcea 0%,#f1da36 100%); /* Chrome10+,Safari5.1+ */
+	background: -o-linear-gradient(top, #fefcea 0%,#f1da36 100%); /* Opera 11.10+ */
+	background: -ms-linear-gradient(top, #fefcea 0%,#f1da36 100%); /* IE10+ */
+	background: linear-gradient(to bottom, #fefcea 0%,#f1da36 100%); /* W3C */
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fefcea', endColorstr='#f1da36',GradientType=0 ); /* IE6-9 */
+}
+</style>
 <html>
   <head>
     <title>Taken World</title>
@@ -20,7 +46,8 @@ include("user_stats.php");
     
     &nbsp; <div id="yourrank"><?php echo "$rrank" ?></div> &nbsp; 
     &nbsp; <div id="username"><?php echo $user['username']; ?></div> &nbsp;
-    <div id="yourenergy"> &nbsp; <b> <?php echo $stats['energy']; ?> / 100 Energy </b> &nbsp; </div>
+    <div id="yourenergy">Energy:</div>
+    <div class="outterprogressbar"> <div class="energyprogress"><?php echo $currentenergy ?> / 100 </div></div>
     <div id="yourgold"> &nbsp; <b> <img src="images/gold_icon1.png" alt="Logo" width="22" height="21"  title="Gold"/><?php echo number_format($stats['gold']); ?> + <?php echo number_format($income); ?> Per Turn </b> &nbsp;</div>
     <div id="yourfood"> &nbsp; <b> <img src="images/food_icon.png" alt="Logo" width="27" height="23"  title="food"/><?php echo number_format($stats['food']); ?> + <?php echo number_format($farming); ?> Per Turn </b> &nbsp;</div>
     <div id="yourcapacity"> &nbsp; <b> Capacity: <?php echo number_format($capacity); ?> </b> &nbsp;</div>
