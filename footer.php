912f85513ad13e902d7e8372847f71f03bccda42
AidanMolloy <aidan@molloy.ie>
2013-07-12 14:15:04 +0100

    1.7.8 12/07/2013

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 </div></div>
     </div>
     <div id="footer"><center><a href="contact.php" title="Contact">Contact</a></center>
-      <div align="right">Copyright &copy; 2013 Taken World</div>
+      <div align="left">Copyright &copy; 2013 Taken World</div>
+      <div id="facebook"><a href="https://www.facebook.com/takenworldgame"><img src="images/facebook_logo.png" width="40" height="40" alt="facebook_logo" /></a></div>
+      <div id="twitter"><a href="https://twitter.com/TakenWorldGame"><img src="images/twitter_logo.png" width="40" height="40" alt="twitter_logo" /></a></div>
     </div>
   </body>
 </html>
